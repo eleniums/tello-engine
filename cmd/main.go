f9cf3b36a97ef3f8bedc0dd6f5be7969a64728c1
eleniums <eleniums@yahoo.com>
2018-10-10 15:48:58 -0700

    Add code to initialize the lua engine and run a script

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/eleniums/tello-engine/engine"
+	"github.com/yuin/gopher-lua"
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/platforms/dji/tello"
 	"gobot.io/x/gobot/platforms/keyboard"
 )
 
 func main() {
+	// get script command-line argument
+	if len(os.Args) < 2 {
+		log.Fatalln("need to pass lua script as command-line argument")
+	}
+	script := os.Args[1]
+
+	// prepare to handle input and start the drone
 	drone := tello.NewDriver("8888")
 	keys := keyboard.NewDriver()
 
@@ -23,5 +34,12 @@ func main() {
 		},
 	)
 
-	robot.Start()
+	robot.Start(false)
+
+	// initialize lua engine and run the script
+	L := lua.NewState()
+	defer L.Close()
+	if err := L.DoFile(script); err != nil {
+		log.Fatalf("error while running lua script: %v", err)
+	}
 }
