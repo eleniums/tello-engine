c001d8d67af8ab32464572ebe2401753e75f4e5a
eleniums <eleniums@yahoo.com>
2018-10-10 21:18:42 -0700

    Add method to check for errors and log

--- engine/common.go
+++ engine/common.go
@@ -14,3 +14,10 @@ func Sleep(milliseconds int) {
 func Log(line string) {
 	log.Printf("(lua) %s", line)
 }
+
+// check is a helper method to reduce clutter and log errors.
+func check(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
--- engine/movement.go
+++ engine/movement.go
@@ -1,36 +1,33 @@
 package engine
 
 import (
-	"log"
 	"time"
 )
 
 // TakeOff will start the blades and raise the drone to a normal flying height.
 func (e *Engine) TakeOff() {
-	err := e.drone.TakeOff()
-	if err != nil {
-		log.Println(err)
-	}
+	check(e.drone.TakeOff())
 	time.Sleep(5000 * time.Millisecond)
 }
 
 // Land will lower the drone to the ground and stop the blades.
 func (e *Engine) Land() {
-	err := e.drone.Land()
-	if err != nil {
-		log.Println(err)
-	}
+	check(e.drone.Land())
 	time.Sleep(5000 * time.Millisecond)
+}
+
+// Forward will move the drone forward at the given speed.
+func (e *Engine) Forward(speed int) {
+	check(e.drone.Forward(speed))
 }
 
 // ResetMovement will set all drone movement to 0.
 func (e *Engine) ResetMovement() {
-	log.Println("Reset drone position")
-	e.drone.Forward(0)
-	e.drone.Backward(0)
-	e.drone.Up(0)
-	e.drone.Down(0)
-	e.drone.Left(0)
-	e.drone.Right(0)
-	e.drone.Clockwise(0)
+	check(e.drone.Forward(0))
+	check(e.drone.Backward(0))
+	check(e.drone.Up(0))
+	check(e.drone.Down(0))
+	check(e.drone.Left(0))
+	check(e.drone.Right(0))
+	check(e.drone.Clockwise(0))
 }
