1d572fe4f16082407d93ce6cd940df26468909bb
eleniums <eleniums@yahoo.com>
2018-10-11 13:31:53 -0700

    Update demo script

--- scripts/demo.lua
+++ scripts/demo.lua
@@ -1,19 +1,32 @@
-speed = 20
-log("set speed to " .. speed)
-
 log("initiating take off")
 takeoff()
 log("hovering")
 
+sleep(1000)
+
+log("fly in a circle")
+forward(20)
+rotateleft(70)
+sleep(6500)
+stop()
+
+sleep(1000)
+
 log("front flip")
 frontflip()
-
 log("back flip")
 backflip()
 
+log("fly in a circle")
+backward(20)
+rotateright(70)
+sleep(6500)
+stop()
+
+sleep(1000)
+
 log("left flip")
 leftflip()
-
 log("right flip")
 rightflip()
 
