b9fb44b1ee0d8b6dceba9a453a7db4c504dce060
eleniums <eleniums@yahoo.com>
2018-10-13 17:13:20 -0700

    Remove calls to reset movement before going in opposite direction
    Turns out these calls are redundant as the same variables are getting set for opposing directions.

--- engine/movement.go
+++ engine/movement.go
@@ -23,49 +23,43 @@ func (e *Engine) Land() {
 // Forward will move the drone forward at the given speed.
 func (e *Engine) Forward(speed int) {
 	debug("Forward, speed: %v", speed)
-	check(e.drone.Backward(0))
 	check(e.drone.Forward(speed))
 }
 
 // Backward will move the drone backward at the given speed.
 func (e *Engine) Backward(speed int) {
 	debug("Backward, speed: %v", speed)
-	check(e.drone.Forward(0))
 	check(e.drone.Backward(speed))
 }
 
 // Left will move the drone left at the given speed.
 func (e *Engine) Left(speed int) {
 	debug("Left, speed: %v", speed)
-	check(e.drone.Right(0))
 	check(e.drone.Left(speed))
 }
 
 // Right will move the drone right at the given speed.
 func (e *Engine) Right(speed int) {
 	debug("Right, speed: %v", speed)
-	check(e.drone.Left(0))
 	check(e.drone.Right(speed))
 }
 
 // Up will move the drone up at the given speed.
 func (e *Engine) Up(speed int) {
 	debug("Up, speed: %v", speed)
-	check(e.drone.Down(0))
 	check(e.drone.Up(speed))
 }
 
 // Down will move the drone down at the given speed.
 func (e *Engine) Down(speed int) {
 	debug("Down, speed: %v", speed)
-	check(e.drone.Up(0))
 	check(e.drone.Down(speed))
 }
 
 // RotateLeft will rotate the drone left at the given speed.
 func (e *Engine) RotateLeft(speed int) {
 	debug("Rotate left, speed: %v", speed)
-	check(e.drone.Clockwise(-speed))
+	check(e.drone.CounterClockwise(speed))
 }
 
 // RotateRight will rotate the drone right at the given speed.
@@ -112,10 +106,7 @@ func (e *Engine) Bounce() {
 func (e *Engine) ResetMovement() {
 	debug("Reset movement")
 	check(e.drone.Forward(0))
-	check(e.drone.Backward(0))
 	check(e.drone.Up(0))
-	check(e.drone.Down(0))
-	check(e.drone.Left(0))
 	check(e.drone.Right(0))
 	check(e.drone.Clockwise(0))
 }
