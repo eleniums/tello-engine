8a77a95b7d5922aebc0f6bf9ac06546f0f4f3373
eleniums <eleniums@yahoo.com>
2019-07-27 11:19:15 -0700

    Start work on go only version

---
+++ cmd/go/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/eleniums/tello-engine/engine"
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+)
+
+func main() {
+	flag.BoolVar(&engine.Debug, "debug", false, "true to enable debug logging")
+	flag.Parse()
+
+	// prepare to handle input and start the drone
+	e := engine.NewEngine()
+	e.Start(false)
+	defer e.Stop()
+
+	// open window for receiving keyboard input
+	win := run()
+
+	for !win.Closed() {
+		win.Clear(colornames.Whitesmoke)
+		win.Update()
+	}
+}
+
+func run() *pixelgl.Window {
+	cfg := pixelgl.WindowConfig{
+		Title:  "tello-go",
+		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	win.SetSmooth(true)
+}
